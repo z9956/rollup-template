2187c66a94272c55ce6d830e15980f8f4ad849ba
9956 <zhz995617@gmail.com>
2021-07-07 22:59:58 +0800

    chore: add TypeScript configuration

--- rollup.config.js
+++ rollup.config.js
@@ -2,21 +2,33 @@ import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 
 const production = !process.env.ROLLUP_WATCH;
 
+const plugins = [
+	resolve(),
+	json(),
+	typescript(),
+	babel({ babelHelpers: 'bundled' }),
+	production && terser(),
+];
+
 export default {
-	input: 'src/main.js',
+	input: 'src/main.ts',
 	output: [
 		{
-			dir: 'es',
-			format: 'es',
+			file: 'dist/main.ems.js',
+			format: 'esm',
+			sourcemap: true,
 		},
 		{
-			dir: 'lib',
-			format: 'cjs',
+			file: 'dist/main.umd.js',
+			format: 'umd',
+			name: 'main',
+			sourcemap: true,
 		},
 	],
-	plugins: [resolve(), json(), babel(), production && terser()],
+	plugins,
 	exclude: 'node_modules',
 };
