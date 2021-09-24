e52b5cceffce7e0f8dda136454ca3a06ee038c26
z9956 <zhz995617@gmail.com>
2021-09-24 17:53:30 +0800

    chore: 更新项目配置

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import json from '@rollup/plugin-json';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -9,26 +10,32 @@ const production = !process.env.ROLLUP_WATCH;
 const plugins = [
 	resolve(),
 	json(),
-	typescript(),
+	commonjs({
+		include: /node_modules/,
+	}),
+	typescript({ tsconfig: './tsconfig.json' }),
 	babel({ babelHelpers: 'bundled' }),
 	production && terser(),
 ];
+
+const pkg = require('./package.json');
+const libraryName = pkg.name;
 
 export default {
 	input: 'src/main.ts',
 	output: [
 		{
-			file: 'dist/main.ems.js',
+			file: `dist/${libraryName}.ems.js`,
 			format: 'esm',
+			name: libraryName,
 			sourcemap: true,
 		},
 		{
-			file: 'dist/main.umd.js',
+			file: `dist/${libraryName}.umd.js`,
 			format: 'umd',
-			name: 'main',
+			name: libraryName,
 			sourcemap: true,
 		},
 	],
 	plugins,
-	exclude: 'node_modules',
 };
